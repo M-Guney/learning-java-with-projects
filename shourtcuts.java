babed45d5e12cff1664750b268d7ead69caf52da
M-Guney <129216053+M-Guney@users.noreply.github.com>
2023-10-28 09:57:44 +0300

    Intellij idea shortscut

---
+++ shourtcuts.java
@@ -0,0 +1,43 @@
+package shortcut;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class shourtcuts {
+    // this shortscut for Intellij idea
+    /*
+    * Belirli satıra gitme      CTRL  + G
+    *
+    *Aynı sayfada klavye ile hareket etme
+    * CTRL page up / down/left/right
+    * hizli kelime secmek icinde kullanabilirsin ctrl yi (ctrl sag /sol)
+    *
+    * Bir sonraki methoda gecme (alt asagi /yukari)
+    *
+    * ALT +1 sol tarafdaki pencereyi kapatır
+    *
+    *
+    *
+    *******onemli*****
+    *** CTRL + SHİFT + ENTER kodun sonundaki kısmı tamamlar ; { ne gerekirse onu tamamlar
+        class ahmet {
+        }
+        if () {
+        }
+    *
+    *
+    * ALT + ENTER       kod önerisinde bulunur
+    *
+    * F2 sonraki errora goturur
+    *
+    *  CTRL + / satiri yorum satirina cevirir
+    *                       geri almak icin tekrardan ctrl / yap
+    *
+    *Ctrl alt shift T butun kısayollari gosterir
+    *
+    *
+    * */
+    public static void main(String[] args) {
+        System.out.println("slm");
+    }
+}
+
